eb7f8d0dd4f860bd088ca03541bb51add96da6da
John Greep <john.greep@sjrb.ca>
2015-02-13 11:32:34 -0700

    Move js to separate file

---
+++ js/main.js
@@ -0,0 +1,13 @@
+    $(document).ready(function(){
+    	// Javascript to enable link to tab
+		var url = document.location.toString();
+		if (url.match('#')) {
+		    $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+		    window.scrollTo(0, 0);
+		} 
+
+		// Change hash for page-reload
+		$('.nav-tabs a').on('shown.bs.tab', function (e) {
+		    window.location.hash = e.target.hash;
+		});
+	});
